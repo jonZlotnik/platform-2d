cdd81e7865d3d552f6436016317ca1a6ee7c9ab6
Jon Zlotnik <j.zlotnik@live.ca>
2018-04-30 22:59:44 -0400

    no message

--- platform-2d/Assets/scripts/Controllable.cs
+++ platform-2d/Assets/scripts/Controllable.cs
@@ -4,18 +4,25 @@ using UnityEngine;
 
 public interface Controllable
 {
-	float maxSpeed {
-		get;
-	}
-	float speed {
-		get;
-	}
-	float jumpHeight {
-		get;
-	}
+	float maxSpeed { get; }
+	float speed { get; }
+	float jumpHeight { get; }
+	int direction { get; }
+	Vector2 position { get; }
 
+	bool isRunning { get; }
+	bool isJumping { get; }
+	bool isGrounded { get; }
+	bool isIdle { get; }
 
+	void updateDirection ();
+	void updatePosition ();
 
+	void moveForward ();
+	void flip ();
+	void jump ();
+	void run ();
+	void walk ();
 
 
 
--- platform-2d/Assets/scripts/Player.cs
+++ platform-2d/Assets/scripts/Player.cs
@@ -2,15 +2,71 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour, Controllable, Combatable {
+public class Player : MonoBehaviour {
+
+
+
+
+	//--------------------------------------------------------
+	// MonoBehaviour
+	//
+
 
 	// Use this for initialization
 	void Start () {
-		
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
+
+	}
+
+
+
+
+
+
+
+
+
+	//------------------------------------------------------------
+	// Controllable
+	//
+
+	private float maxSpeed { get; }
+	private float speed { get; }
+	private float jumpHeight { get; }
+	private int direction { get; }
+	private Vector2 position { get; }
+
+	private bool isRunning { get; }
+	private bool isJumping { get; }
+	private bool isGrounded { get; }
+	private bool isIdle { get; }
+
+	void updateDirection ()
+	{
+		this.direction = (int)Mathf.Sign(this.transform.localScale.x);
+	}
+	void updatePosition ()
+	{
+		this.position.Set (this.position.x, this.position.y);
+	}
+
+	void moveForward ()
+	{
 		
 	}
+	void flip ();
+	void jump ();
+	void run ();
+	void walk ();
+
+
+
+
+
+
+
 }
